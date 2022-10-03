4e14ad6a36740402525904270ce17362071da1cd
Mahyaaa <94241597+raulmahya123@users.noreply.github.com>
2022-10-03 18:50:11 +0700

    Add files via upload

---
+++ testing/testing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MENU PERSEGI PANJANG");
+            Console.WriteLine("1. Hitung luas");
+            Console.WriteLine("2. Hitung luas keliling");
+
+            int pilih;
+            Console.WriteLine("Menupilihan");
+            pilih = int.Parse(Console.ReadLine());
+
+            if (pilih >= 3 || pilih >= 5)
+            {
+                Console.WriteLine("menu tidak sesuai");
+            }
+
+            double panjang,tinggi,hasil;
+            Console.WriteLine("=== MENGHITUNG LUAS PERSEGI PANJANG  =");
+            Console.Write("Masukkan panjang (cm) : ");
+            panjang = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan tinggi (cm)  : ");
+            tinggi = double.Parse(Console.ReadLine());
+            hasil =  panjang * tinggi;
+            Console.WriteLine("\n\tHasilnya, luas persegi panjang adalah : {0} cm", hasil);
+
+
+            Console.WriteLine("Ingin Mengulang Kembali (Y/T");
+            Console.ReadLine();
+        }
+    }
+}
